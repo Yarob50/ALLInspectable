1c813c2aa1f8c0cb01b5d12f51cb4478edc3e5c7
Yarob Al Mostafa <yarob.hm@gmail.com>
2020-08-07 19:05:12 +0300

    Initial commit

---
+++ ALLInspectable/ALLInspectable.h
@@ -0,0 +1,19 @@
+//
+//  ALLInspectable.h
+//  ALLInspectable
+//
+//  Created by يعرب المصطفى on 8/7/20.
+//  Copyright © 2020 yarob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALLInspectable.
+FOUNDATION_EXPORT double ALLInspectableVersionNumber;
+
+//! Project version string for ALLInspectable.
+FOUNDATION_EXPORT const unsigned char ALLInspectableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALLInspectable/PublicHeader.h>
+
+
---
+++ ALLInspectableExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALLInspectableExample
+//
+//  Created by يعرب المصطفى on 8/7/20.
+//  Copyright © 2020 yarob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
